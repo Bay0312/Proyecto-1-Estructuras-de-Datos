9d4c9c51f9e2b2be136d13f1a1f4b2997be38332
AndreyHB <andrey.hernandez.benavides@est.una.ac.cr>
2024-09-27 18:34:45 -0600

    PruebasUnitarias

---
+++ Proyecto 1 - Estructuras de Datos/Pruebas Unitarias/Pruebas Unitarias.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PruebasUnitarias
+{
+	TEST_CLASS(PruebasUnitarias)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
